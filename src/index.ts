a32f0580e4c782e56400e2dbb8db6e88af3ef8dc
LucasFreije <92063968+LucasFreije@users.noreply.github.com>
2021-10-06 21:33:20 -0300

    se agrego contador "I"

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 console.log("que capo, ya estoy usando github y voy a hacer mi primer commit");
 console.log("Este seria mi primer commit");
 console.log("Sigo jugando con los commit");
+let i: number = 0;
+console.log(i);
