ef4e09a4879b0c10f41fc2d06c1fa20569530d09
LucasFreije <92063968+LucasFreije@users.noreply.github.com>
2021-10-06 21:35:07 -0300

    se agrego numero "J" y se sumaron

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ console.log("Este seria mi primer commit");
 console.log("Sigo jugando con los commit");
 let i: number = 0;
 console.log(i);
+let j: number = 5;
+console.log(i + j + " esta es la suma de los 2 numeros");
